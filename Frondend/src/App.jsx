d711e40552b45c02dec0db90276e6e625f6a73e7
Priyangshu Bhunia <priyangshubhunia03@gmail.com>
2025-05-05 23:21:01 +0530

    Frondend updeated , Backend  started

--- Frondend/src/App.jsx
+++ Frondend/src/App.jsx
@@ -1,15 +1,36 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import "prismjs/themes/prism-tomorrow.css";
+import Editor from "react-simple-code-editor";
+import prism from "prismjs"
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  // const [count, setCount] = useState(0)
+  const [code, setCode] = useState(`function sum() {return 1 + 1}`);
 
+  useEffect(()=>{
+    prism.highlightAll();
+  },[]);
   return (
     <>
     <main>
       <div className="laft">
-        <div className="code"></div>
+        <div className="code">
+          <Editor>
+            value{code}
+            onValueChange = {code => setCode(code)}
+            highlight ={code => prism.highlight(code, prism.languages.javascript, "javascript")}
+            padding ={10}
+            style={{
+              fontFamily :"fira code ,",
+              fontSize : 16,
+              border : "1px solid #ddd",
+              borderRedius: "5px",
+              height: "100%",
+              width: "100%"
+            }}
+          </Editor>
+        </div>
         <div className="review">review</div>
       </div>
       <div className="right"></div>
